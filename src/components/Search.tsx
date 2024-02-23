e8bf93818680e3d33da38db8726dec9cc9870d33
haruki <meteo.haru@gmail.com>
2024-02-23 13:25:46 +0000

    fix style error

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,15 +7,15 @@ export interface SearchProps {
 export default function Search(props: SearchProps) {
     const [search, setSearch] = createSignal("");
     return (
-        <div class="flex items-center w-full rounded-l-full p-0.5 rounded-r-full border-neutral-400 border">
+        <div class="flex items-center w-full bg-neutral-100 rounded-l-full p-0.5 rounded-r-full border-neutral-400 border">
             <input
                 type="text"
                 value={search()}
-                class="py-2 px-4 rounded-l-full h-full flex-1 block outline-0 bg-neutral-200"
+                class="py-2 px-4 rounded-l-full h-full flex-1 block outline-0 bg-none"
                 onInput={(e) => {setSearch(e.currentTarget.value)}}
             />
             <button
-                class="hover:border-primary border-primary-flat inline border-2 duration-200 bg-primary-flat text-primary py-2 px-3 rounded-r-full h-full"
+                class="hover:border-primary border-primary-flat inline border duration-200 bg-primary-flat text-primary py-2 px-3 rounded-r-full h-full"
                 onClick={() => props.onSearch?.("")}
             >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
